6f6cb19423807df42b28479c4fa881fe3fe79939
Vitalii_Semkov <vitalii_semkov@epam.com>
2019-05-06 18:54:08 +0300

    temp

--- complete/src/main/java/app/service/impl/AsyncService.java
+++ complete/src/main/java/app/service/impl/AsyncService.java
@@ -1,9 +1,14 @@
 package app.service.impl;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AsyncService {
 
+    @Scheduled(fixedRate = 1000)
+    public void doInSchedule() {
+        System.err.println("Schedule");
+    }
 
 }
